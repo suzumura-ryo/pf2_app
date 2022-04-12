1f16ff03b1a3f4c9a778b5a9992beed70aa9b9fb
Ubuntu <ubuntu@ip-172-31-7-199.ap-northeast-1.compute.internal>
2022-04-12 08:41:15 +0000

    “発展的ログイン実装完了”

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
     p  params
     p "============================"
 
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_path
     else
         flash.now[:danger] = "メールまたはパスワードが間違っています"
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_010116) do
+ActiveRecord::Schema.define(version: 2022_04_11_144049) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_010116) do
     t.integer "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
